9ddba36b0344e5041d43f77810dd2d4d9b004a06
LAPTOP-19827398\PC <sang05112003@gmail.com>
2025-09-16 02:33:41 +0700

    refactor: add more valid headers into middleware cors

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -26,7 +26,7 @@ export class CorsMiddleware implements NestMiddleware {
     );
     res.setHeader(
       'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie',
+      'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie, x-skip-refresh, x-request-id',
     );
     res.setHeader('Access-Control-Expose-Headers', 'Set-Cookie');
 
